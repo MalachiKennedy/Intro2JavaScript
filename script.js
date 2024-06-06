30375384c257ac7db892e0ad8eb765f5c5c0911e
Malachi <144715076+MalachiKennedy@users.noreply.github.com>
2024-06-06 21:21:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function myfunction() {
+document.getElementById("greet").style.color="blue";
+}
+
+function myfunction2() {
+document.querySelector("body").style.backgroundColor="purple";
+}
+
+function changeFont() {
+  document.querySelector("p").style.fontFamily = "fantasy";
+}
